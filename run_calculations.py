4dee95fad35b9ca9089489419d2f069363efece0
Igor Gaidai <gaidai92@gmail.com>
2025-09-22 12:27:19 -0400

    Adds power flow problem

--- run_calculations.py
+++ run_calculations.py
@@ -1,18 +1,30 @@
 import time
 
 import numpy as np
+import qiskit
+from networkx import Graph
 from numpy import random
+from qiskit import QuantumCircuit
 from qiskit.primitives import StatevectorSampler
+from qiskit_ionq import IonQProvider
 
 from src.CircuitLayer import AllToAllEntangler, ZXMixer
-from src.PowerGrid import GeneratorCommitmentProblem, Generator, CostFunction
-from src.Sampler import ExactSampler, MySamplerV2
+from src.PowerGrid import GeneratorCommitmentProblem, Generator, PowerFlowProblem
+from src.Sampler import ExactSampler, MySamplerV2, IonQSampler
 from src.VariationalCircuit import VariationalCircuit
 
 
-def get_problem() -> GeneratorCommitmentProblem:
+def get_generator_commitment_problem() -> GeneratorCommitmentProblem:
     generators = np.array([Generator((15, 20), (0, 1, 10)),
-                           Generator((0, 10), (1, 0, 1))])
+                           Generator((0, 10), (1, 0, 1)),
+                           Generator((0, 10), (2, 0, 1)),
+                           Generator((0, 10), (3, 0, 1)),
+                           Generator((0, 10), (4, 0, 1)),
+                           Generator((0, 10), (5, 0, 1)),
+                           Generator((0, 10), (6, 0, 1)),
+                           Generator((0, 10), (7, 0, 1)),
+                           Generator((0, 10), (8, 0, 1)),
+                           Generator((0, 10), (9, 0, 1))])
     load = 10
 
     # generators = np.array([Generator((100, 600), (0.002, 10, 500)),
@@ -37,30 +49,56 @@ def get_problem() -> GeneratorCommitmentProblem:
     return problem
 
 
+def get_power_flow_problem() -> PowerFlowProblem:
+    graph = Graph()
+    graph.add_node(0, generators=[Generator((0, 300), (0.01, 10, 10))], load=0)
+    graph.add_node(1, generators=[Generator((0, 100), (0.02, 20, 1))], load=100)
+    graph.add_node(2, generators=[], load=50)
+
+    graph.add_edge(0, 1, capacity=100)
+    graph.add_edge(0, 2, capacity=100)
+    graph.add_edge(1, 2, capacity=100)
+    return PowerFlowProblem(graph)
+
+
+def get_job_cost(circuit: QuantumCircuit, nfev: int) -> tuple[int, int, int, float]:
+    native_backend = IonQProvider().get_backend("qpu.forte-1", gateset="native")
+    circuit_transpiled = qiskit.transpile(circuit, native_backend)
+    num_one_qubit_gates = sum(1 for instruction in circuit_transpiled.data if instruction.operation.num_qubits == 1)
+    num_two_qubit_gates = sum(1 for instruction in circuit_transpiled.data if instruction.operation.num_qubits == 2)
+    return num_one_qubit_gates, num_two_qubit_gates, nfev, (num_one_qubit_gates * 0.0001645 + num_two_qubit_gates * 0.0011213) * nfev
+
+
 def main():
-    problem = get_problem()
+    problem = get_generator_commitment_problem()
+    # problem = get_power_flow_problem()
     num_gen = len(problem.generators)
-    cost_function = CostFunction(problem)
 
     entangler = AllToAllEntangler(num_gen)
     mixer = ZXMixer(num_gen)
-    # sampler = ExactSampler()
-    sampler = MySamplerV2(StatevectorSampler(default_shots=1))
     num_layers = 1
 
-    vqa = VariationalCircuit(num_layers, [entangler, mixer], sampler)
-    initial_angles = random.uniform(-np.pi, np.pi, len(vqa.circuit.parameters))
-    result = vqa.optimize_parameters(cost_function.evaluate, initial_angles)
+    # sampler = ExactSampler()
+    sampler = MySamplerV2(StatevectorSampler(default_shots=1))
+    # sampler = IonQSampler("simulator", 1000, None)
+    # sampler = IonQSampler("qpu.forte-1", 1000, None)
 
-    best_sample = min(cost_function.known_values.items(), key=lambda pair: pair[1].fun)
+    seed = 0
+    rng = random.default_rng(seed)
+
+    vqa = VariationalCircuit(num_layers, [entangler, mixer], sampler)
+    initial_angles = rng.uniform(-np.pi, np.pi, len(vqa.circuit.parameters))
+    result = vqa.optimize_parameters(problem.evaluate, initial_angles)
+
+    best_sample = min(problem.optimize_power.cache.items(), key=lambda pair: pair[1].total)
     print(f"Angle optimization successful: {result.success}")
-    print(f"State-average cost: {result.fun}")
+    print(f"State-average cost: {result.total}")
     print("=== Best sample ===")
     print(f"Power optimization successful: {best_sample[1].success}")
     print(f"Generators selected: {best_sample[0]}")
     print(f"Optimized power: {best_sample[1].x}")
-    print(f"Optimized cost: {best_sample[1].generation_cost}")
-    print(f"Constraint: {best_sample[1].constraint}")
+    print(f"Optimized cost: {best_sample[1].fun}")
+    print(f"Penalty: {best_sample[1].penalty}")
 
 
 if __name__ == "__main__":
--- src/PowerGrid.py
+++ src/PowerGrid.py
@@ -1,10 +1,35 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+from functools import wraps, partial
+from typing import Callable, Hashable, Any
 
+import networkx as nx
+import numpy as np
 from networkx import Graph
-from numpy import ndarray
-from numpy import random
+from numpy.typing import NDArray
 from scipy import optimize
 from scipy.optimize import OptimizeResult
+
+
+def cached[K: Hashable, T](func: Callable[[K], T]) -> Callable[[K], T]:
+    """ Function decorator that saves evaluation results in function's internal dict and reads them if the function is called again with the same argument. """
+    @wraps(func)
+    def wrapper(arg: K) -> T:
+        if arg not in wrapper.cache:
+            wrapper.cache[arg] = func(arg)
+        return wrapper.cache[arg]
+
+    wrapper.cache = {}
+    return wrapper
+
+
+def get_penalty(powers: list[float], constraints: list[dict[str, Any]], mult: float = 1e1) -> float:
+    """ Evaluates penalty term for a given power vector and list of constraints. """
+    penalty = 0
+    for constraint in constraints:
+        val = constraint["fun"](powers)
+        if val < 0:
+            penalty += mult * val ** 2
+    return penalty
 
 
 @dataclass
@@ -25,49 +50,91 @@ class Generator:
 class GeneratorCommitmentProblem:
     """ Describes a unit commitment problem in a power grid.
     I.e. given a set of generators, which ones should be enabled and at what power in order to meet target load using the smallest operation cost. """
-    generators: ndarray[Generator]
+    generators: NDArray[Generator]
     load: float
 
-
-@dataclass
-class PowerFlowProblem:
-    """ Generalized version of GeneratorCommitmentProblem, where locations of generators and loads are taken into account.
-    Specifically, the problem is described by a graph, where nodes represent neighborhoods that can include generators and loads.
-    Generated power is consumed by local loads, but any excess power can be transferred to adjacent nodes to supplement their generators or be passed further in the network.
-    Edges represent power lines between neighborhoods and have finite capacities, so routing the generated power to the loads now becomes a problem too. """
-    grid: Graph
-
-
-@dataclass
-class CostFunction:
-    """ Describes power grid cost function. Evaluates and stores known results. """
-    problem: GeneratorCommitmentProblem
-    known_values: dict[str, OptimizeResult] = field(default_factory=dict)
+    def __post_init__(self):
+        self.optimize_power = cached(self.optimize_power)
 
     def optimize_power(self, bitstring: str) -> OptimizeResult:
-        def generation_cost(powers: list[float]) -> float:
+        """ Finds optimal generation cost for a given generator assignment. """
+        def generation_cost_total(powers: list[float]) -> float:
             return sum(gen.generation_cost(power) for gen, power in zip(enabled_generators, powers))
 
-        def generation_cost_total(powers: list[float]) -> float:
-            cost = generation_cost(powers)
-            constraint_dist = constraint["fun"](powers)
-            if constraint_dist < 0:
-                penalty = 1e1 * constraint_dist ** 2
-                cost += penalty
-            return cost
-
-        enabled_generators = self.problem.generators[[int(val) == 1 for val in bitstring]]
-        initial_point = [random.uniform(*gen.power_range) for gen in enabled_generators]
+        enabled_generators = self.generators[[int(val) == 1 for val in bitstring]]
+        initial_point = [gen.power_range[1] for gen in enabled_generators]
         bounds = [gen.power_range for gen in enabled_generators] if enabled_generators.size > 0 else None
-        constraint = {"type": "ineq", "fun": lambda powers: sum(powers) - self.problem.load}
-        result = optimize.minimize(generation_cost_total, initial_point, method="SLSQP", bounds=bounds, constraints=constraint)
-        result.generation_cost = generation_cost(result.x)
-        result.constraint = constraint["fun"](result.x)
+        constraints = [{"type": "ineq", "fun": lambda powers: sum(powers) - self.load}]
+        result = optimize.minimize(generation_cost_total, initial_point, method="SLSQP", bounds=bounds, constraints=constraints)
+        result.penalty = get_penalty(result.x, constraints)
+        result.total = result.fun + result.penalty
         return result
 
     def evaluate(self, bitstring: str) -> float:
-        if bitstring not in self.known_values:
-            self.known_values[bitstring] = self.optimize_power(bitstring)
-        return self.known_values[bitstring].fun
+        return self.optimize_power(bitstring).total
 
 
+class PowerFlowProblem:
+    """ Generalized version of GeneratorCommitmentProblem, where locations of generators and loads are taken into account.
+    Specifically, the problem is described by a graph, where nodes represent neighborhoods that can include generators and loads.
+    Generated power is consumed by local loads. Any excess power can be transferred to adjacent nodes to supplement their generators.
+    Edges represent power lines between neighborhoods and have finite capacities, so routing the generated power to the loads now becomes a problem too. """
+
+    def __init__(self, graph: Graph):
+        """ Graph nodes should have the following properties:
+        1) generators: list[Generator]. List of generator instances located at a given node.
+        2) load: float >= 0. Total load at a given node.
+        Graph edges should have the following properties:
+        1) capacity: float > 0. Maximum power that can be routed through a given edge.
+        The following additional properties will be automatically added to the graph:
+        1) var_inds: list[int], added to nodes. List of indices in the optimization vector to which generator's power outputs at this node map.
+        2) var_ind: int, added to edges. Index in the optimization vector to which power flow through this edge maps.
+        3) start: node key type, added to edges. Defines positive flow direction by choosing an arbitrary end of a given edge as start.
+        Collects generators from all nodes into a single generators list. """
+        self.graph = graph
+        self.generators = np.array([gen for _, gens in self.graph.nodes(data="generators") for gen in gens])
+        self.optimize_power = cached(self.optimize_power)
+
+        var_ind = 0
+        for _, data in self.graph.nodes(data=True):
+            data["var_inds"] = list(range(var_ind, var_ind + len(data["generators"])))
+            var_ind += len(data["generators"])
+        nx.set_edge_attributes(self.graph, {(u, v): {"var_ind": i + var_ind, "start": u} for i, (u, v) in enumerate(self.graph.edges)})
+
+    def evaluate_power_balance(self, powers: list[float], node_label: Hashable) -> float:
+        """ Evaluates power balance at a given node, i.e. sum of all generated powers + incoming - outgoing - load. """
+        power_balance = 0
+        this_node = self.graph.nodes[node_label]
+        for gen_ind in this_node["var_inds"]:
+            power_balance += powers[gen_ind]
+        for _, v, data in self.graph.edges(node_label, data=True):
+            power_balance += powers[data["var_ind"]] * (-1) ** (node_label == data["start"])
+        power_balance -= this_node["load"]
+        return power_balance
+
+    def get_constraints(self) -> list[dict[str, Any]]:
+        """ Provides a list of inequality constraints. Each constraint requires power balance at a given node to be non-negative.
+        Positive power balance (i.e. excess power) is allowed, since it is assumed that each node has a power sink. """
+        constraints = []
+        for label in self.graph.nodes:
+            constraints.append({"type": "ineq", "fun": partial(self.evaluate_power_balance, node_label=label)})
+        return constraints
+
+    def optimize_power(self, bitstring: str) -> OptimizeResult:
+        """ Finds optimal power vector for a given set of enabled generators, defined by the bitstring. """
+        def generation_cost_total(powers: list[float]) -> float:
+            return sum(gen.generation_cost(power) for gen, power in zip(self.generators, powers))
+
+        bounds = [np.array(gen.power_range) * int(bitstring[i]) for i, gen in enumerate(self.generators)]
+        bounds += [(-cap, cap) for _, _, cap in self.graph.edges(data="capacity")]
+        initial_point = [bound[1] for bound in bounds[:len(self.generators)]]
+        initial_point += [0] * len(self.graph.edges)
+        constraints = self.get_constraints()
+        result = optimize.minimize(generation_cost_total, initial_point, method="SLSQP", bounds=bounds, constraints=constraints)
+        result.penalty = get_penalty(result.x, constraints)
+        result.total = result.fun + result.penalty
+        return result
+
+    def evaluate(self, bitstring: str) -> float:
+        """ Returns optimal generation cost + penalty for a given set of enabled generators. """
+        return self.optimize_power(bitstring).total
