743f659e04d33891be6f33d6c6bfd89689e09bed
Igor Gaidai <gaidai92@gmail.com>
2025-09-22 12:28:53 -0400

    Adds IonQ sampler

--- src/Sampler.py
+++ src/Sampler.py
@@ -5,6 +5,7 @@ from typing import Sequence
 from qiskit import QuantumCircuit
 from qiskit.primitives import BaseSamplerV2
 from qiskit.quantum_info import Statevector
+from qiskit_ionq import IonQProvider
 
 
 class Sampler(ABC):
@@ -34,3 +35,21 @@ class MySamplerV2(Sampler):
         counts = self.sampler.run([(measured_circuit, param_vals)]).result()[0].data.meas.get_counts()
         probabilities = {key: value / self.sampler.default_shots for key, value in counts.items()}
         return probabilities
+
+
+class IonQSampler(Sampler):
+    """ Uses IonQ's hardware or cloud simulators to get probability distribution. """
+
+    def __init__(self, backend_name: str, shots: int = 1000, noise_model: str = None):
+        self.backend_name = backend_name
+        self.backend = IonQProvider().get_backend(backend_name)
+        self.shots = shots
+        if noise_model is not None:
+            self.backend.set_options(noise_model=noise_model)
+
+    def get_sample_probabilities(self, circuit: QuantumCircuit, param_vals: Sequence[float]) -> dict[str, float]:
+        bound = circuit.assign_parameters(param_vals)
+        result = self.backend.run(bound, shots=self.shots).result()
+        counts = result.get_counts()
+        counts = {key.rjust(circuit.num_qubits, "0"): value / self.shots for key, value in counts.items()}
+        return counts
